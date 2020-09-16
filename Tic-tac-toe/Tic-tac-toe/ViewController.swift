63a61550deae084ef13de7179a1f52c503770e96
Rastislav Smolen <>
2020-09-16 14:29:58 +0100

    Commit 1 : Base build

---
+++ Tic-tac-toe/Tic-tac-toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic-tac-toe
+//
+//  Created by Rastislav Smolen on 16/09/2020.
+//  Copyright © 2020 Rastislav Smolen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
